b32636a4a84d99c57bf30d577ca0003c03199c8f
Lucas Farias <lukf95@gmail.com>
2023-08-05 09:59:17 +0200

    Save

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,13 +1,5 @@
+import config from './packages/packlify-prettier/config';
+
 module.exports = {
-  trailingComma: 'es5',
-  printWidth: 120,
-  arrowParens: 'avoid',
-  singleQuote: true,
-  importOrder: ['^@/(.*)$', '^[./]'],
-  importOrderSeparation: false,
-  importOrderSortSpecifiers: true,
-  importOrderGroupNamespaceSpecifiers: true,
-  importOrderCaseInsensitive: true,
-  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
-  pluginSearchDirs: false,
+  ...config,
 };
