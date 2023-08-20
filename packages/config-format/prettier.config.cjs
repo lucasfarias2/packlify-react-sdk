dea4d68129b22a23683bc8298b9d684713094dfd
Lucas Farias <lukf95@gmail.com>
2023-08-20 12:10:02 +0200

    Fix config

--- packages/config-format/prettier.config.cjs
+++ packages/config-format/prettier.config.cjs
@@ -8,5 +8,6 @@ module.exports = {
   importOrderSortSpecifiers: true,
   importOrderGroupNamespaceSpecifiers: true,
   importOrderCaseInsensitive: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
   pluginSearchDirs: false,
 };
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,5 @@
-const config = require('./packages/config-format')
+const config = require('./packages/config-format/prettier.config.cjs');
 
 module.exports = {
-  ...config
+  ...config,
 };
