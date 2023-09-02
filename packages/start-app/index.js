7e1bcedc002fe9d1d383562f0acafe7ad698e03f
Lucas Farias <lukf95@gmail.com>
2023-09-02 23:49:39 +0200

    Creating eslint files and p.json

--- packages/start-app/index.js
+++ packages/start-app/index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 import chalk from 'chalk';
+import fs from 'fs';
+import path from 'path';
 import readline from 'readline';
+import url from 'url';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 console.log(chalk.bold.hex('#0e7490')('Packlify: ') + chalk.bold.hex('#be123c')('start-app'));
 
@@ -12,19 +17,55 @@ const startApp = () => {
 
   rl.question('Name for the project? ', projectName => {
     if (projectName) {
-
       if (!fs.existsSync(projectName)) {
         fs.mkdirSync(projectName);
       }
 
-      fs.writeFileSync(`${projectName}/tuvieja.js`, 'console.log("asd");');
-      
+      createPackageJson(projectName);
+      createEslintConfig(projectName);
+      createIndexFile(projectName);
+
       console.log(`Project ${projectName} has been created!`);
     } else {
       console.log('Please provide a valid project name!');
     }
     rl.close();
   });
+};
+
+const createPackageJson = projectName => {
+  const packageJsonContent = {
+    name: projectName,
+    version: '1.0.0',
+    type: 'module',
+    main: 'index.js',
+    scripts: {
+      format: 'packlify-format-write',
+      'format:check': 'packlify-format-check',
+      lint: 'packlify-lint',
+    },
+    license: 'MIT',
+    devDependencies: {
+      '@packlify/config-format': '0.0.13-alpha.0',
+      'eslint-config-packlify': '0.0.4',
+    },
+  };
+
+  fs.writeFileSync(`./${projectName}/package.json`, JSON.stringify(packageJsonContent, null, 2));
+};
+
+const createEslintConfig = projectName => {
+  const src = path.join(__dirname, './files/.eslintrc.cjs');
+  const tgt = path.join(process.cwd(), projectName, '.eslintrc.cjs');
+
+  fs.copyFileSync(src, tgt);
+};
+
+const createIndexFile = projectName => {
+  const src = path.join(__dirname, './files/index.js');
+  const tgt = path.join(process.cwd(), projectName, 'index.js');
+
+  fs.copyFileSync(src, tgt);
 };
 
 startApp();
