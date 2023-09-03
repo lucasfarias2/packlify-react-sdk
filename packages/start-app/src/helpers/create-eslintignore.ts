47a7040cb4bb35e26fe05f723ca548138dd0cbdc
Lucas Farias <lukf95@gmail.com>
2023-09-03 11:17:57 +0200

    Fix eslintignore name

--- packages/start-app/src/helpers/create-eslintignore.ts
+++ packages/start-app/src/helpers/create-eslintignore.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import eslintIgnoreTemplate from '../files/eslintignore.js';
 
 export default function createEslintIgnore(projectName: string) {
-  const tgt = path.join(process.cwd(), projectName, 'eslintrcignore');
+  const tgt = path.join(process.cwd(), projectName, '.eslintignore');
 
   fs.writeFileSync(tgt, eslintIgnoreTemplate());
 }
