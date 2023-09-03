9cb8149804bd5f160f5453d6bb9663e87e6698bd
Lucas Farias <lukf95@gmail.com>
2023-09-03 11:22:30 +0200

    Fix gitignore

--- packages/start-app/src/helpers/create-gitignore.ts
+++ packages/start-app/src/helpers/create-gitignore.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import gitIgnoreTemplate from '../files/eslintignore.js';
+import gitIgnoreTemplate from '../files/gitignore.js';
 
 export default function createGitIgnore(projectName: string) {
   const tgt = path.join(process.cwd(), projectName, '.gitignore');
