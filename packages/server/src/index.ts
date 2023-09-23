d4c4afc3186ac5c3b3f529b47416b0df53bd5777
Lucas Farias <lukf95@gmail.com>
2023-09-23 15:14:43 +0200

    Fixes

---
+++ packages/server/src/index.ts
@@ -0,0 +1,6 @@
+import { createServer } from './createServer.js';
+import { hydrate } from './hydrate.js';
+import { renderComponent } from './renderComponent.js';
+import { PacklifyServerRequest, PacklifyServerResponse } from './types.js';
+
+export { createServer, renderComponent, hydrate, PacklifyServerResponse, PacklifyServerRequest };
