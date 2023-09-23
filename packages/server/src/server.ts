365be00f95e9f904af91b3eafb9cf246486a6a46
Lucas Farias <lukf95@gmail.com>
2023-09-23 15:04:22 +0200

    FIX

---
+++ packages/server/src/server.ts
@@ -0,0 +1,5 @@
+import { createServer } from './createServer.js';
+import { renderComponent } from './renderComponent.js';
+import { PacklifyServerRequest, PacklifyServerResponse } from './types.js';
+
+export { createServer, renderComponent, PacklifyServerResponse, PacklifyServerRequest };
