b88adac188a3f2afaa6e648f3b575687734e4471
Lucas Farias <lukf95@gmail.com>
2023-09-23 12:24:02 +0200

    Test to remove two files

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,4 @@
 import { createServer } from './createServer.js';
-import { hydrate } from './hydrate.js';
-import { renderComponent } from './renderComponent.js';
 import { PacklifyServerRequest, PacklifyServerResponse } from './types.js';
 
-export { createServer, renderComponent, hydrate, PacklifyServerResponse, PacklifyServerRequest };
+export { createServer, PacklifyServerResponse, PacklifyServerRequest };
