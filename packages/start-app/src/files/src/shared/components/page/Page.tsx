0832d1ecc1e25ffd8dacf681d3f7a44049c83da2
Lucas Farias <lukf95@gmail.com>
2023-09-29 20:01:02 +0200

    Remove wrong variables and use classnames

--- packages/start-app/src/files/src/shared/components/page/Page.tsx
+++ packages/start-app/src/files/src/shared/components/page/Page.tsx
@@ -1,13 +1,14 @@
 import { DeviceContext } from '@/shared/contexts/DeviceContext';
+import classnames from 'classnames';
 
 const Page = ({ children, className, initialState, device, withNavbar = false, darkMode = false }: IProps) => {
   return (
     <DeviceContext.Provider value={{ type: device?.type }}>
-      <main className={`${className} ${withNavbar ? 'with-navbar' : 'without-navbar'} ${darkMode ? 'dark' : ''}`}>
-        {children}
-      </main>
+      <main className={classnames(className, { dark: darkMode })}>{children}</main>
       <footer
-        className={`h-100 border-t bg-white p-6 text-center text-xs text-gray-400 shadow-sm ${darkMode ? 'dark' : ''}`}
+        className={classnames('h-100 border-t bg-white p-6 text-center text-xs text-gray-400 shadow-sm', {
+          dark: darkMode,
+        })}
       >
         <div>
           Copyright &copy; {new Date().getFullYear()} Lucas Farias. All rights reserved. <p>lucasfarias.com</p>
